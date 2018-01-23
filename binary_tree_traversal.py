9148c0bc8001af9102d1835bb7298bf1d6422af0
cookie <751540262@qq.com>
2018-01-23 19:27:01 +0800

    binary tree traversal

--- binary_tree_traversal.py
+++ binary_tree_traversal.py
@@ -50,27 +50,77 @@ class BinaryTreeTraversal:
         res = []
         stack = []
         while(root or stack):
-
+            if(root):
+                res.append(root.val) # 访问当前节点
+                stack.append(root.right) # 右孩子入栈
+                root = root.left  # 向左走
+            else:
+                root = stack.pop() # 出栈
+        return res
 
     # inorder
     def inorderTraversal_2(self, root):
         res = []
         stack = []
         while(root or stack):
-            while(root):
+            while(root):  # 向左走到头，同时都入栈
                 stack.append(root)
                 root = root.left
-            node = stack.pop()
-            res.append(node)
-            root = node.right
+            node = stack.pop() # 出栈
+            res.append(node.val) # 访问该节点
+            root = node.right # 向右走
         return res
 
+    # postorder
     def postorderTraversal_2(self, root):
+        pass
 
     ##------------------------------------------------------##
     # 3.Morris Traversal
+    # preorder
     def preorderTraversal_3(self, root):
+        res = []
+        while(root):
+            if root.left:
+                # 找到当前节点的前驱节点
+                tmp = root.left
+                while(tmp.right and tmp.right != root):
+                    tmp = tmp.right
 
+                if tmp.right == root: # 如果已经连接过，则重置回去
+                    tmp.right = None
+                    root = root.right
+                else:
+                    tmp.right = root # 如果没有连接过，将前驱节点指向当前节点，并访问当前节点
+                    res.append(root.val)
+                    root = root.left # 往左走
+            else:
+                res.append(root.val) # 访问当前节点
+                root = root.right
+        return res
+
+    # inorder
     def inorderTraversal_3(self, root):
+        res = []
+        while(root):
+            if root.left:
+                # 找到当前节点的前驱节点
+                tmp = root.left
+                while(tmp.right and tmp.right != root):
+                    tmp = tmp.right
 
+                if tmp.right == root: # 如果已经连接过，则重置回去，并访问当前节点
+                    tmp.right = None
+                    res.append(root.val)
+                    root = root.right
+                else:
+                    tmp.right = root # 如果没有连接过，将前驱节点指向当前节点
+                    root = root.left # 往左走
+            else:
+                res.append(root.val) # 访问当前节点
+                root = root.right
+        return res
+
+    # postorder
     def postorderTraversal_3(self, root):
+        pass
